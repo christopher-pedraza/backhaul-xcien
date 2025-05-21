9cbe06d5e1623562107ce0579dc184b7adc90e15
arturodcs <a00833516@tec.mx>
2025-05-21 10:38:12 -0600

    feat: fetch topologies from rtdb

--- backhaul-xcien/src/services/topology.ts
+++ backhaul-xcien/src/services/topology.ts
@@ -1,28 +1,68 @@
 import { Topology } from "@/types/Topology";
-import { TOPOLOGIES } from "@/data";
+import { get, ref } from "firebase/database";
+import { rtdb } from "@/firebaseConfig";
+import { CytoscapeOptions } from "cytoscape";
 
-type TopologyOption = Pick<Topology, "id" | "name">;
 
-export const getTopologyOptions = (): Promise<TopologyOption[]> => {
-  return new Promise(
-    (res) =>
-      setTimeout(
-        () => res(TOPOLOGIES.map(({ id, name }) => ({ id, name }))),
-        300,
-      ), // Simulate a network request
-  );
+interface TopologyOption {
+  id: string;
+  name: string;
+}
+
+export const getTopologyOptions = async (): Promise<TopologyOption[]> => {
+  const snap = await get(ref(rtdb, "topologyIndex"));
+
+  const options: TopologyOption[] = [];
+  console.log(snap);
+  snap.forEach((c) => {
+    const id = c.key;
+    if (!id) return;
+
+    const name = c.val() as string;
+
+    options.push({ id, name })
+  });
+
+  return options;
 };
 
-export const getTopologyById = (id: string): Promise<Topology> => {
-  return new Promise(
-    (res) =>
-      setTimeout(() => {
-        const topology = TOPOLOGIES.find((t) => t.id === id);
-        if (topology) {
-          res(topology);
-        } else {
-          throw new Error("Topology not found");
-        }
-      }, 300), // Simulate a network request
+export const getTopologyById = async (id: string): Promise<Topology> => {
+  const snap = await get(ref(rtdb, `topologies/${id}`));
+
+  if (!snap.exists()) {
+    throw new Error(`Topology "${id}" not found`);
+  }
+
+  const { nodes = {}, edges = {} } = snap.val();
+
+  // convert the nodes
+  const nodeElements: CytoscapeOptions["elements"] = Object.entries(nodes).map(
+    ([nodeId, node]: any) => ({
+      data: {
+        id: nodeId,
+        name: node.name,
+        clients: node.clients ?? {},
+      },
+      position: { x: node.x, y: node.y },
+      classes: node.type,
+    }),
   );
+
+  // convert the edges
+  const edgeElements: CytoscapeOptions["elements"] = Object.entries(edges).map(
+    ([edgeId, edge]: any) => ({
+      data: {
+        id: edgeId,
+        source: edge.source,
+        target: edge.target,
+        capacity: edge.capacity,
+        usage: edge.usage
+      },
+    }),
+  );
+
+  return {
+    id,
+    elements: [...nodeElements, ...edgeElements],
+  };
 };
