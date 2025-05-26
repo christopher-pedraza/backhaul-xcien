5c7bef2f25742403b13cf723f6977ec18e0bb8d8
christopher-pedraza <62347713+christopher-pedraza@users.noreply.github.com>
2025-05-26 17:02:06 -0600

    Add disabled prop to Input component in TabInput for better read-only handling

--- backhaul-xcien/src/components/Sidebar/TabInput/index.tsx
+++ backhaul-xcien/src/components/Sidebar/TabInput/index.tsx
@@ -43,6 +43,7 @@ export default function TabInput({
       classNames={{ input: inputClass }}
       isReadOnly={isReadOnly}
       labelPlacement={labelPlacement}
+      disabled={isReadOnly}
     />
   );
 }
