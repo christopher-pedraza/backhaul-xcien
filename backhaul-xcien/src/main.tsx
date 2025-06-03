d0ff64bcfcb7bb92e0207ec29f65f9a010b9b7a5
arturodcs <a00833516@tec.mx>
2025-06-03 15:58:15 -0600

    feat: add ToastProvider and showSuccessToast utility for enhanced notifications

--- backhaul-xcien/src/main.tsx
+++ backhaul-xcien/src/main.tsx
@@ -9,6 +9,8 @@ import {
 } from "@/providers";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ToastProvider } from "@heroui/toast";
+
 
 
 import "@/styles/globals.css";
@@ -19,6 +21,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <HeroUIProviderWrapper>
+        <ToastProvider />
         <CytoscapeProvider>
           <ChangeLogProvider>
             <QueryClientProvider client={queryClient}>
---
+++ backhaul-xcien/src/utils/toasts.ts
@@ -0,0 +1,17 @@
+import { addToast } from "@heroui/react"
+
+interface ToastOptions {
+  title: string;
+  description?: string;
+}
+
+export const showSuccessToast = ({
+  title,
+  description
+}: ToastOptions) => {
+  addToast({
+    title,
+    description,
+    color: "success",
+  })
+}
